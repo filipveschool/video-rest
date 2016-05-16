296a8f87ee0e2f19097c8c2d4b734ecff97b95bd
filipve <filipve@outlook.com>
2016-05-16 17:48:05 +0200

    crossorigin

--- src/main/java/org/ucll/rest/controller/VideoRestController.java
+++ src/main/java/org/ucll/rest/controller/VideoRestController.java
@@ -8,10 +8,7 @@ import helperclasses.MovieGenre;
 import helperclasses.MovieRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,7 +23,7 @@ public class VideoRestController {
     //VideoBeheer vdb;
 
     VideoBeheer vdb = new VideoBeheer(DatabaseType.RELATIONAL);
-
+    @CrossOrigin
     @RequestMapping(value = "/addMovie",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE)
